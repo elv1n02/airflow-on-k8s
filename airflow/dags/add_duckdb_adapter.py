0d7ec67c6cb9a1fedd3423cc917adbe73f53a8ce
elv1n02 <106514416+elv1n02@users.noreply.github.com>
2025-02-11 17:52:30 +0400

    Update add_duckdb_adapter.py

--- airflow/dags/add_duckdb_adapter.py
+++ airflow/dags/add_duckdb_adapter.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 # Define the DAG
 dag = DAG(
-    'install_dbt',
+    'install_dbt_duckdb_adapter',
     description='An example DAG that runs bash commands',
     schedule_interval='@daily',  # Run daily, adjust as needed
     start_date=datetime(2025, 2, 11),

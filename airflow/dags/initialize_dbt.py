2d830eb47830237122c718b65bd8488f25c78dcb
elv1n02 <106514416+elv1n02@users.noreply.github.com>
2025-02-11 17:49:36 +0400

    Update initialize_dbt.py

--- airflow/dags/initialize_dbt.py
+++ airflow/dags/initialize_dbt.py
@@ -14,7 +14,7 @@ dag = DAG(
 # Define a bash command task
 bash_task = BashOperator(
     task_id='run_bash_command',
-    bash_command='dbt init',  # Your bash command here
+    bash_command='dbt init my_project',  # Your bash command here
     dag=dag,
 )
 

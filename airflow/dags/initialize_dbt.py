71cd1a0ca5b21e65140d0155d7d78fd2bc95247a
elv1n02 <106514416+elv1n02@users.noreply.github.com>
2025-02-11 17:39:48 +0400

    Create initialize_dbt.py

---
+++ airflow/dags/initialize_dbt.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+# Define the DAG
+dag = DAG(
+    'initialize_dbt',
+    description='An example DAG that runs bash commands',
+    schedule_interval='@daily',  # Run daily, adjust as needed
+    start_date=datetime(2025, 2, 11),
+    catchup=False,
+)
+
+# Define a bash command task
+bash_task = BashOperator(
+    task_id='run_bash_command',
+    bash_command='dbt init',  # Your bash command here
+    dag=dag,
+)
+
+bash_task

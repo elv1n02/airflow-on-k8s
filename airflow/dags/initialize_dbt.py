30cb5e46a02da319c171e194c67dd8d4c7980a25
elv1n02 <106514416+elv1n02@users.noreply.github.com>
2025-02-13 17:54:12 +0400

    Update initialize_dbt.py

--- airflow/dags/initialize_dbt.py
+++ airflow/dags/initialize_dbt.py
@@ -14,7 +14,7 @@ dag = DAG(
 # Define a bash command task
 bash_task = BashOperator(
     task_id='run_bash_command',
-    bash_command='cd .. && cd .. && cd .. && cd .. && ls',  # Navigate up 4 directories and then run dbt
+    bash_command='ls',  # Navigate up 4 directories and then run dbt
     dag=dag,
 )
 

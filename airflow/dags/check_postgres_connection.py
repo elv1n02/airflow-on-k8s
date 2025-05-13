e788414ed0cc863f668f8a24c41d8ff674bf1ec9
elv1n02 <106514416+elv1n02@users.noreply.github.com>
2025-05-13 19:10:57 +0400

    Update check_postgres_connection.py

--- airflow/dags/check_postgres_connection.py
+++ airflow/dags/check_postgres_connection.py
@@ -4,12 +4,6 @@ from airflow.models import Variable
 from datetime import datetime
 import os
 
-GITHUB_USERNAME = 'yourusername'
-GITHUB_REPO = 'your-repo'
-GITHUB_TOKEN = Variable.get('GITHUB_TOKEN')  # Securely stored token
-CLONE_DIR = '/tmp/cloned_repo'
-REPO_URL = f'https://{GITHUB_USERNAME}:{GITHUB_TOKEN}@github.com/{GITHUB_USERNAME}/{GITHUB_REPO}.git'
-
 default_args = {
     'start_date': datetime(2024, 1, 1),
 }
@@ -22,24 +16,14 @@ with DAG(
     description='Install Git, clone private GitHub repo, install requirements, and run script',
 ) as dag:
 
-    install_git = BashOperator(
-        task_id='install_git',
-        bash_command='apt-get update && apt-get install -y git',
-    )
-
-    clone_repo = BashOperator(
-        task_id='clone_github_repo',
-        bash_command=f'rm -rf {CLONE_DIR} && git clone {REPO_URL} {CLONE_DIR}',
-    )
-
     install_requirements = BashOperator(
         task_id='install_requirements',
-        bash_command=f'pip install -r {os.path.join(CLONE_DIR, "requirements.txt")}',
+        bash_command=f'pip install -r {os.path.join("requirements.txt")}',
     )
 
     run_script = BashOperator(
         task_id='run_script',
-        bash_command=f'python {os.path.join(CLONE_DIR, "run.py")}',
+        bash_command=f'python {os.path.join("run.py")}',
     )
 
-    install_git >> clone_repo >> install_requirements >> run_script
+    install_requirements >> run_script

6f05e1558fa5b2edfe728b70cbf1f3a47b786a9f
elv1n02 <106514416+elv1n02@users.noreply.github.com>
2025-05-13 19:27:39 +0400

    Update check_postgres_connection.py

--- airflow/dags/check_postgres_connection.py
+++ airflow/dags/check_postgres_connection.py
@@ -6,7 +6,7 @@ import os
 
 DAG_DIR = os.path.dirname(os.path.abspath(__file__))
 REQUIREMENTS_PATH = os.path.abspath(os.path.join(DAG_DIR, 'requirements.txt'))
-RUN_PATH = os.path.abspath(os.path.join(DAG_DIR, '..', 'run.py'))
+RUN_PATH = os.path.abspath(os.path.join(DAG_DIR, 'run.py'))
 
 default_args = {
     'start_date': datetime(2024, 1, 1),

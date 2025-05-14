802b80e403840d2b1d647bdcb4c5868176385fdb
elv1n02 <106514416+elv1n02@users.noreply.github.com>
2025-05-14 14:30:06 +0400

    Update run.py

--- airflow/dags/run.py
+++ airflow/dags/run.py
@@ -1,15 +1,15 @@
 import great_expectations as gx
 import os
 
-print(os.path.dirname(os.path.abspath(__file__)))
+os.mkdirs('/opt/gx-dev', exist_ok = True)
 
-DAG_DIR = os.path.dirname(os.path.abspath(__file__))
+# DAG_DIR = os.path.dirname(os.path.abspath(__file__))
 
-context = gx.get_context(mode='file', project_root_dir=DAG_DIR)
-print(context.checkpoints.all())
+# context = gx.get_context(mode='file', project_root_dir=DAG_DIR)
+# print(context.checkpoints.all())
 
-checkpoint = context.checkpoints.get("my_checkpoint")
+# checkpoint = context.checkpoints.get("my_checkpoint")
 
-validation_results = checkpoint.run()
+# validation_results = checkpoint.run()
 
-print(validation_results)
+# print(validation_results)

7e3926761bd369ed31cd1fd7dd3bffc6cd760c80
elv1n02 <106514416+elv1n02@users.noreply.github.com>
2025-05-14 14:57:07 +0400

    Update check_postgres_connection.py

--- airflow/dags/check_postgres_connection.py
+++ airflow/dags/check_postgres_connection.py
@@ -1,64 +1,41 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from airflow.models import Variable
 from datetime import datetime
-import os
-
-DAG_DIR = os.path.dirname(os.path.abspath(__file__))
-GX_DIR = os.path.abspath(os.path.join(DAG_DIR, 'gx'))
-REQUIREMENTS_PATH = os.path.abspath(os.path.join(DAG_DIR, 'requirements.txt'))
-RUN_PATH = os.path.abspath(os.path.join(DAG_DIR, 'run.py'))
+from airflow.models import Variable
 
 default_args = {
     'start_date': datetime(2024, 1, 1),
 }
 
+GITHUB_PAT = Variable.get("GITHUB_TOKEN")
+REPO = "elv1n02/gx-dev"  # e.g., elvinmammadov/my-private-repo
+BRANCH = "main"  # or whichever branch you want
+
+REPO_URL = f"https://{GITHUB_PAT}:x-oauth-basic@github.com/{REPO}.git"
+
 with DAG(
-    dag_id='private_github_clone_install_run',
+    dag_id='clone_private_git_and_run_python',
     default_args=default_args,
     schedule_interval=None,
     catchup=False,
-    description='Install Git, clone private GitHub repo, install requirements, and run script',
 ) as dag:
 
-    write_in_pod = KubernetesPodOperator(
-        task_id="create_files_dirs",
-        name="create-files-dirs",
-        namespace="gx-dev",
-        image="python:3.10-slim",
+    run_git_python = KubernetesPodOperator(
+        task_id="clone_and_run",
+        name="git-run",
+        namespace="default",
+        image="python:3.10",  # Has git + pip
         cmds=["bash", "-c"],
         arguments=[
-            """
-            echo "Current directory: $(pwd)" && \
-            mkdir -p /tmp/test_dir && \
-            echo "Hello from pod" > /tmp/test_dir/hello.txt && \
-            cat /tmp/test_dir/hello.txt
+            f"""
+            apt-get update && apt-get install -y git && \
+            git clone --single-branch --branch {BRANCH} {REPO_URL} repo && \
+            cd repo && \
+            pip install -r requirements.txt && \
+            python3 run.py
             """
         ],
         is_delete_operator_pod=True,
         get_logs=True,
         in_cluster=True,
     )
-    
-    print_dir = BashOperator(
-        task_id='print_dir',
-        bash_command=f'echo "DAG_DIR: {DAG_DIR}"',
-    )
-
-    print_path = BashOperator(
-        task_id='print_path',
-        bash_command=f'echo "REQUIREMENTS_PATH: {REQUIREMENTS_PATH}"',
-    )
-
-    install_requirements = BashOperator(
-        task_id='install_requirements',
-        bash_command=f'pip install -r {REQUIREMENTS_PATH}',
-    )
-
-    run_script = BashOperator(
-        task_id='run_script',
-        bash_command=f'python3 {RUN_PATH}',
-    )
-
-    write_in_pod
